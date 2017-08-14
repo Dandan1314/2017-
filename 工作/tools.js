a18cda101cb7b474dcdd8634c0e62e8bc63859b2
buguan37201 <1601939476@qq.com>
2017-08-14 18:06:06 +0800

    updata

--- 工作/tools.js
+++ 工作/tools.js
@@ -42,5 +42,77 @@ function getTargetObj(key) {
     $.getTargetObj(key);
     $.showFocusBorder();
 }
+/*时间类*/
+//是否是闰年
+Date.prototype.isLeapYear = function () {
+    return (0==this.getYear()%4&&((this.getYear()%100!=0)||(this.getYear()%400==0)));
+}
+//把日期分割成数组
+Date.prototype.toArray = function() {
+    var myDate = this;
+    var myArray = Array();
+    myArray[0] = myDate.getFullYear();
+    myArray[1] = myDate.getMonth();
+    myArray[2] = myDate.getDate();
+    myArray[3] = myDate.getHours();
+    myArray[4] = myDate.getMinutes();
+    myArray[5] = myDate.getSeconds();
+    return myArray;
+}
+//日期计算
+Date.prototype.DateAdd = function(strInterval, Number) {
+    var dtTmp = this;
+    switch (strInterval) {
+        case 's' :return new Date(Date.parse(dtTmp) + (1000 * Number));
+        case 'n' :return new Date(Date.parse(dtTmp) + (60000 * Number));
+        case 'h' :return new Date(Date.parse(dtTmp) + (3600000 * Number));
+        case 'd' :return new Date(Date.parse(dtTmp) + (86400000 * Number));
+        case 'w' :return new Date(Date.parse(dtTmp) + ((86400000 * 7) * Number));
+        case 'q' :return new Date(dtTmp.getFullYear(), (dtTmp.getMonth()) + Number*3, dtTmp.getDate(), dtTmp.getHours(), dtTmp.getMinutes(), dtTmp.getSeconds());
+        case 'm' :return new Date(dtTmp.getFullYear(), (dtTmp.getMonth()) + Number, dtTmp.getDate(), dtTmp.getHours(), dtTmp.getMinutes(), dtTmp.getSeconds());
+        case 'y' :return new Date((dtTmp.getFullYear() + Number), dtTmp.getMonth(), dtTmp.getDate(), dtTmp.getHours(), dtTmp.getMinutes(), dtTmp.getSeconds());
+    }
+}
+//日期差
+Date.prototype.DateDiff = function(strInterval, dtEnd) {
+    var dtStart = this;
+    if (typeof dtEnd == 'string' )//如果是字符串转换为日期型
+    {
+        dtEnd = StringToDate(dtEnd);
+    }
+    switch (strInterval) {
+        case 's' :return parseInt((dtEnd - dtStart) / 1000);
+        case 'n' :return parseInt((dtEnd - dtStart) / 60000);
+        case 'h' :return parseInt((dtEnd - dtStart) / 3600000);
+        case 'd' :return parseInt((dtEnd - dtStart) / 86400000);
+        case 'w' :return parseInt((dtEnd - dtStart) / (86400000 * 7));
+        case 'm' :return (dtEnd.getMonth()+1)+((dtEnd.getFullYear()-dtStart.getFullYear())*12) - (dtStart.getMonth()+1);
+        case 'y' :return dtEnd.getFullYear() - dtStart.getFullYear();
+    }
+}
+//| 字符串转成日期类型
+//| 格式 MM/dd/YYYY MM-dd-YYYY YYYY/MM/dd YYYY-MM-dd
+//+---------------------------------------------------
+function StringToDate(DateStr)
+{
 
+    var converted = Date.parse(DateStr);
+    var myDate = new Date(converted);
+    if (isNaN(myDate))
+    {
+        //var delimCahar = DateStr.indexOf('/')!=-1?'/':'-';
+        var arys= DateStr.split('-');
+        myDate = new Date(arys[0],--arys[1],arys[2]);
+    }
+    return myDate;
+}
+//判断当前月的天数
+Date.prototype.MaxDayOfDate = function() {
+    var myDate = this;
+    var ary = myDate.toArray();
+    var date1 = (new Date(ary[0],ary[1]+1,1));
+    var date2 = date1.dateAdd(1,'m',1);
+    var result = dateDiff(date1.Format('yyyy-MM-dd'),date2.Format('yyyy-MM-dd'));
+    return result;
+}
 
